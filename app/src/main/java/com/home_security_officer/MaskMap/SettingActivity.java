b6d47f7bf2eac100be01f891769fb568ac4a3d00
Heeseung Jang <jang5934@gmail.com>
2020-05-16 13:39:22 +0900

    Sixth commit. Displaying detail information of seller by clicking marker's info window is added. and some trivial updates are adapted. + some trivial bugs are fixed.

--- app/src/main/java/com/home_security_officer/MaskMap/SettingActivity.java
+++ app/src/main/java/com/home_security_officer/MaskMap/SettingActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -23,6 +30,19 @@ public class SettingActivity extends AppCompatActivity {
         Intent intent = getIntent();
         EditText editText = findViewById(R.id.edit_range);
         editText.setText(intent.getStringExtra("range"));
+
+        TextView hyperLink = findViewById(R.id.check_rule_hyper_link);
+        SpannableString content = new SpannableString("앱 마켓 모바일콘텐츠 결제 가이드라인 확인하기");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        hyperLink.setText(content);
+
+        Linkify.TransformFilter mTransform = new Linkify.TransformFilter() {
+            @Override public String transformUrl(Matcher match, String url) {
+                return "";
+            }
+        };
+        Pattern pattern1 = Pattern.compile("앱 마켓 모바일콘텐츠 결제 가이드라인 확인하기");
+        Linkify.addLinks(hyperLink, pattern1, "https://github.com/jang5934/MaskStockLookUp/raw/master/mobile.pdf",null,mTransform);
     }
 
     @Override
